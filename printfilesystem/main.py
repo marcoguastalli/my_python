aa2946b9e382fb918196824df45b83a3ba24393d
marcoguastalli <marco.guastalli@gmail.com>
2021-01-01 22:25:17 +0100

    added try catch

--- printfilesystem/main.py
+++ printfilesystem/main.py
@@ -1,3 +1,5 @@
+import shutil
+
 from printfilesystem.create.create_json import CreateJson
 from printfilesystem.read.read_folder import ReadFolder
 from printfilesystem.read.read_json import ReadJson
@@ -6,13 +8,13 @@ from printfilesystem.utils.py_utils import create_folder_if_not_exist
 
 
 def main():
-    source_path = "/home/marco27/Downloads"
+    source_path = "/media/marco27/Data/DiscoD"
     target_path = "/home/marco27/temp/json"
     create_folder_if_not_exist(target_path)
 
     rf = ReadFolder(source_path)
     files_in_folder = rf.read_files_in_folder_using_os()
-    print("The folder with path '%s' contains %s paths" % (source_path, files_in_folder.__len__()))
+    print("The folder with path '%s' contains %s files" % (source_path, files_in_folder.__len__()))
 
     create_json = CreateJson(files_in_folder, target_path)
     create_json.create()
@@ -22,11 +24,14 @@ def main():
     json_model_in_folder = rj.get_json_model()
     print("The folder with path '%s' contains %s json files" % (target_path, json_model_in_folder.__len__()))
 
+    # remove directory
+    shutil.rmtree(target_path, ignore_errors=False, onerror=None)
+
     for json_model in json_model_in_folder:
         sj = StoreJson(json_model)
         stored_json = sj.store()
-        print("Stored json '%s'" % stored_json)
-    print("Stored json from folder '%s'" % target_path)
+        # print("Stored json '%s'" % stored_json)
+    print("Stored json from folder '%s'" % source_path)
 
     exit(0)
 
--- printfilesystem/store/store__json.py
+++ printfilesystem/store/store__json.py
@@ -7,10 +7,13 @@ class StoreJson:
 
     def store(self):
         json_as_sting = str(self.json_model)
-
         url = 'http://localhost:8980/marco27-web/v1/pfs/create'
         headers = {'Content-type': 'application/json', 'Accept': '*/*'}
-        response = requests.post(url, data=json_as_sting, headers=headers)
+        try:
+            response = requests.post(url, data=json_as_sting, headers=headers)
+        except Exception as e:
+            print("Error store json:\n %s" % json_as_sting)
+            print(e)
 
         result = json_as_sting
         return result
