fabafac594068d966d68d146771badbbbb3b68a8
marcoguastalli <marco.guastalli@gmail.com>
2021-01-03 21:59:16 +0100

    {'path: ': '/media/marco27/Data/DiscoD/video/moviesToWatchAndDelete/2011 - A Few Best Men.mp4', 'mime_type': [': video/mp4'], 'duration': [': 1 hours 36 min 31 sec'], 'width': [': 1280 pixels'], 'height': [': 536 pixels'], 'creation_date': [': 2015-08-22 13:33:24'], 'last_modification': [': 2015-08-22 13:33:24'], 'comment': [': Play speed: 100.0%', ': User volume: 100.0%'], 'endian': [': Big endian']}

--- metadata-hachoir/src/metadata_hachoir.py
+++ metadata-hachoir/src/metadata_hachoir.py
@@ -16,6 +16,14 @@ def extract_metadata_from_video(source_path):
             result['duration'] = metadata.getValues('duration')
             result['width'] = metadata.getValues('width')
             result['height'] = metadata.getValues('height')
+        for data in sorted(metadata, key=lambda data: data.priority):
+            if not data.values:
+                continue
+            text = []
+            for item in data.values:
+                value = item.text
+                text.append(": %s" % value)
+            result[data.key] = text
         return result
     except Exception as err:
         print("Metadata extraction error: %s" % err)
