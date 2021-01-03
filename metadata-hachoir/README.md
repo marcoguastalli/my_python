994fce8459ef26dc70d0a8b9d6610b30e23b2e56
marcoguastalli <marco.guastalli@gmail.com>
2021-01-03 21:51:14 +0100

    {'path: ': '/media/marco27/Data/DiscoD/video/moviesToWatchAndDelete/2011 - A Few Best Men.mp4', 'mime_type': ['video/mp4'], 'duration': [datetime.timedelta(seconds=5791, microseconds=458333)], 'width': [1280], 'height': [536]}

--- metadata-hachoir/README.md
+++ metadata-hachoir/README.md
@@ -3,6 +3,7 @@ here some links inspired me to write the src code
 
 https://github.com/vstinner/hachoir
 https://hachoir.readthedocs.io/en/latest/install.html
+https://hachoir.readthedocs.io/en/latest/developer.html
 https://hachoir.readthedocs.io/en/latest/parser.html
 https://hachoir.readthedocs.io/en/latest/metadata.html
 
@@ -11,3 +12,15 @@ cd ~/dev/repository/git/my_python/metadata-hachoir/src
 python3 -m pip install -U hachoir
 python3 -m pip install -U urwid
 python3 -m pip install -U wxPython
+
+###### metadata
+Metadata:
+- Duration: 1 hours 36 min 31 sec
+- Image width: 1280 pixels
+- Image height: 536 pixels
+- Creation date: 2015-08-22 13:33:24
+- Last modification: 2015-08-22 13:33:24
+- Comment: Play speed: 100.0%
+- Comment: User volume: 100.0%
+- MIME type: video/mp4
+- Endianness: Big endian
--- metadata-hachoir/src/metadata_hachoir.py
+++ metadata-hachoir/src/metadata_hachoir.py
@@ -1,14 +1,22 @@
 from hachoir.metadata import extractMetadata
+from hachoir.metadata.metadata import Metadata
 from hachoir.parser import createParser
 
 
 def extract_metadata_from_video(source_path):
+    result = dict({'path: ': source_path})
     try:
         parser = createParser(source_path)
         if not parser:
-            return dict({'no parser for path: ': source_path})
-        metadata = extractMetadata(parser)
-        return metadata
+            result['result'] = 'Parses is None'
+            return result
+        metadata: Metadata = extractMetadata(parser)
+        if metadata:
+            result['mime_type'] = metadata.getValues('mime_type')
+            result['duration'] = metadata.getValues('duration')
+            result['width'] = metadata.getValues('width')
+            result['height'] = metadata.getValues('height')
+        return result
     except Exception as err:
         print("Metadata extraction error: %s" % err)
         return None
