510946952e36422331397b858c4d83d97d3f310f
marcoguastalli <marco.guastalli@gmail.com>
2021-01-03 21:29:36 +0100

    Metadata: - Duration: 1 hours 36 min 31 sec - Image width: 1280 pixels - Image height: 536 pixels - Creation date: 2015-08-22 13:33:24 - Last modification: 2015-08-22 13:33:24 - Comment: Play speed: 100.0% - Comment: User volume: 100.0% - MIME type: video/mp4 - Endianness: Big endian

--- metadata-hachoir/README.md
+++ metadata-hachoir/README.md
@@ -3,6 +3,8 @@ here some links inspired me to write the src code
 
 https://github.com/vstinner/hachoir
 https://hachoir.readthedocs.io/en/latest/install.html
+https://hachoir.readthedocs.io/en/latest/parser.html
+https://hachoir.readthedocs.io/en/latest/metadata.html
 
 ### install
 cd ~/dev/repository/git/my_python/metadata-hachoir/src
--- metadata-hachoir/src/metadata_hachoir.py
+++ metadata-hachoir/src/metadata_hachoir.py
@@ -1,9 +1,14 @@
+from hachoir.metadata import extractMetadata
 from hachoir.parser import createParser
 
 
 def extract_metadata_from_video(source_path):
-    parser = createParser(source_path)
-    if not parser:
+    try:
+        parser = createParser(source_path)
+        if not parser:
+            return dict({'no parser for path: ': source_path})
+        metadata = extractMetadata(parser)
+        return metadata
+    except Exception as err:
+        print("Metadata extraction error: %s" % err)
         return None
-    result = dict({'path': source_path, 'mime_type': str(parser.mime_type)})
-    return result
