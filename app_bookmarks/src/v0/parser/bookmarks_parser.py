fb01a3af49c99e8b975c7e72d1edb713b8e17886
marcoguastalli <marco.guastalli@netcentric.biz>
2021-03-11 13:06:21 +0100

    print all the href

--- app_bookmarks/src/v0/parser/bookmarks_parser.py
+++ app_bookmarks/src/v0/parser/bookmarks_parser.py
@@ -8,11 +8,17 @@ class ParseBookmarksHtmlFile:
         self.bookmarks_html_file = bookmarks_html_file
 
     def parse_bookmarks_html_file(self):
-        with open(self.bookmarks_html_file) as file_to_parse:
-            soup = get_soup_from_html(file_to_parse)
-            tags = get_tags_from_soup(soup, 'a')
+        tags = self.get_a()
+        for a in tags:
+            print(a['href'])
 
         result = []
         bookmarks = Bookmarks("DuckDuck", "http:", "dir")
         result.append(bookmarks)
+        return result
+
+    def get_a(self):
+        with open(self.bookmarks_html_file) as file_to_parse:
+            soup = get_soup_from_html(file_to_parse)
+            result = get_tags_from_soup(soup, 'a')
         return result
