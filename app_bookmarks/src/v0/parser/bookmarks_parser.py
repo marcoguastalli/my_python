a1765b311d2f266e7762c28fa26ac9fa43310d40
marcoguastalli <marco.guastalli@netcentric.biz>
2021-03-12 10:36:37 +0100

    added created_modified_date

--- app_bookmarks/src/v0/parser/bookmarks_parser.py
+++ app_bookmarks/src/v0/parser/bookmarks_parser.py
@@ -1,6 +1,7 @@
 from app_bookmarks.src.v0.model.bookmarks_model import Bookmarks
 from html_parser.html_parser import get_soup_from_html
 from html_parser.html_parser import get_tags_from_soup
+from utils.py_utils_date import convert_timestamp_to_str_with_format
 
 
 class ParseBookmarksHtmlFile:
@@ -12,10 +13,13 @@ class ParseBookmarksHtmlFile:
         tags = self.get_a()
         for a in tags:
             href = a['href']
-            add_date = a['add_date']
             title = a.contents[0]
+            add_date = a['add_date']
+            created_modified_date = convert_timestamp_to_str_with_format(add_date)
+
             bookmarks = Bookmarks(title, href, "TODO")
-            bookmarks.set_created(add_date)
+            bookmarks.set_created(created_modified_date)
+            bookmarks.set_modified(created_modified_date)
             result.append(bookmarks)
         return result
 
---
+++ utils/py_utils_date_test.py
@@ -0,0 +1,48 @@
+import unittest
+from datetime import datetime
+
+import py_utils_date
+
+
+class Test(unittest.TestSuite):
+    class Tests(unittest.TestCase):
+
+        def test_get_current_datetime_with_format_as_string(self):
+            date_format = "%Y-%m-%d %H:%M:%S"
+            actual = py_utils_date.get_current_datetime_with_format_as_string(date_format)
+            self.assertIsInstance(actual, str)
+            self.assertIsNotNone(actual)
+            # self.assertEqual(str('2021-03-12 10:04'), actual)
+
+        def test_convert_datetime_to_timestamp(self):
+            date_time = datetime(1975, 12, 27, 13, 14, 15)
+            actual = py_utils_date.convert_datetime_to_timestamp(date_time)
+            self.assertIsInstance(actual, float)
+            self.assertIsNotNone(actual)
+            self.assertEqual(float(188914455), actual)
+
+        def test_convert_timestamp_to_datetime(self):
+            time_stamp = 188914455
+            actual = py_utils_date.convert_timestamp_to_datetime(time_stamp)
+            self.assertIsInstance(actual, datetime)
+            self.assertIsNotNone(actual)
+            self.assertEqual(datetime(1975, 12, 27, 13, 14, 15), actual)
+
+        def test_convert_timestamp_to_str_with_format(self):
+            time_stamp = 188914455
+            date_format = "%Y-%m-%d %H:%M:%S"
+            actual = py_utils_date.convert_timestamp_to_str_with_format(time_stamp, date_format)
+            self.assertIsInstance(actual, str)
+            self.assertIsNotNone(actual)
+            self.assertEqual(str('1975-12-27 13:14:15'), actual)
+
+            time_stamp: str = "188914455"
+            date_format = "%Y-%m-%d %H:%M:%S"
+            actual = py_utils_date.convert_timestamp_to_str_with_format(time_stamp, date_format)
+            self.assertIsInstance(actual, str)
+            self.assertIsNotNone(actual)
+            self.assertEqual(str('1975-12-27 13:14:15'), actual)
+
+
+if __name__ == "__main__":
+    unittest.main(Test)
