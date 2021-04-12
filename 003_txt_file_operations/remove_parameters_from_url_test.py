d7a00c457b8f29df953cb68dfe686d549601e842
marcoguastalli <marco.guastalli@netcentric.biz>
2021-04-12 18:14:44 +0200

    added environent variables

--- 003_txt_file_operations/remove_parameters_from_url_test.py
+++ 003_txt_file_operations/remove_parameters_from_url_test.py
@@ -1,3 +1,4 @@
+import os
 import unittest
 
 from remove_parameters_from_url import remove_parameters_from_url_in_file
@@ -7,8 +8,8 @@ class Test(unittest.TestSuite):
     class Tests(unittest.TestCase):
 
         def test_remove_parameters_from_url_in_file(self):
-            source_file_name = "/home/marco27/Downloads/todo_insta.txt"
-            target_file_name = "/home/marco27/Downloads/todo_insta_filtered.txt"
+            source_file_name = os.environ['MY_HOME'] + "/Downloads/todo_insta.txt"
+            target_file_name = os.environ['MY_HOME'] + "/Downloads/todo_insta_filtered.txt"
             self.assertIsNotNone(self, remove_parameters_from_url_in_file(source_file_name, target_file_name))
 
 
