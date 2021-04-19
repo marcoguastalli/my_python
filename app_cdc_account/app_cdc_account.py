d956a5292acc08eed47fb710836c7d098f4d35b6
marcoguastalli <marco.guastalli@netcentric.biz>
2021-04-19 15:45:59 +0200

    error 500

--- app_cdc_account/app_cdc_account.py
+++ app_cdc_account/app_cdc_account.py
@@ -3,17 +3,18 @@ from utils.py_utils_date import get_current_datetime_as_int
 
 REST_API_ENDPOINT_SANDBOX = "https://uat-api.3ona.co/v2/"
 REST_API_ENDPOINT_PRODUCTION = "https://api.crypto.com/v2/"
-REST_API_ENDPOINT = REST_API_ENDPOINT_SANDBOX
+REST_API_ENDPOINT = REST_API_ENDPOINT_PRODUCTION
 REST_API_KEY = ""
 REST_API_SECRET = ""
 
 
 def main():
-    print("Reading account from API url '%s'" % REST_API_ENDPOINT)
+    url = REST_API_ENDPOINT + "private/get-account-summary"
     current_timestamp = get_current_datetime_as_int()
+    print("Reading account from API url '%s' at '%s'" % (REST_API_ENDPOINT, current_timestamp))
 
-    account_summary = GetAccountSummary(REST_API_ENDPOINT, REST_API_KEY, REST_API_SECRET)
-    response = account_summary.do_post("27", "CRO")
+    account_summary = GetAccountSummary(url, REST_API_KEY, REST_API_SECRET)
+    response = account_summary.do_post("1", "CRO")
     print(response)
 
 
