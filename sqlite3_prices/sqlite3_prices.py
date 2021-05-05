f69d3fc8a7c515a34ef9dda348b656730d07139e
marcoguastalli <marco.guastalli@netcentric.biz>
2021-05-05 21:43:37 +0200

    drop create insert select

--- sqlite3_prices/sqlite3_prices.py
+++ sqlite3_prices/sqlite3_prices.py
@@ -8,23 +8,30 @@ from execute_query import execute_query
 def main():
     database = "/Users/marcoguastalli/opt/sqlite/my_python_prices.sqlite"
 
-    sql_create_stocks_table = '''CREATE TABLE IF NOT EXISTS prices
-                                (id integer PRIMARY KEY
-                                symbol text NOT NULL, 
-                                amount real NOT NULL, 
-                                created text NOT NULL)'''
-
     conn = create_connection(database)
     try:
         if conn is not None:
+            # drop table
+            sql_drop_table = "DROP TABLE prices"
+            execute_query(conn, sql_drop_table)
+            conn.commit()
             # create table
-            execute_query(conn, sql_create_stocks_table)
+            sql_create_table = '''CREATE TABLE IF NOT EXISTS prices
+                                        (id integer PRIMARY KEY,
+                                        symbol text NOT NULL, 
+                                        amount real NOT NULL, 
+                                        created text NOT NULL)'''
+            execute_query(conn, sql_create_table)
             conn.commit()
             # first insert
             query_insert = "INSERT INTO prices VALUES (1, 'BTC', 48000.14, '2021-05-05')"
             execute_query(conn, query_insert)
-            # commit
             conn.commit()
+            # select
+            query_select = "SELECT * FROM prices ORDER BY id"
+            cursor = conn.cursor()
+            for row in cursor.execute(query_select):
+                print(row)
         else:
             print("Error Connection to DDBB:" + database)
     finally:
