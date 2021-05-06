0aaf21f98751653b02beecb662456d1dcde292fb
marcoguastalli <marco.guastalli@netcentric.biz>
2021-05-06 22:37:53 +0200

    SELECT * FROM prices ORDER BY created DESC

--- sqlite3_prices/sqlite3_prices.py
+++ sqlite3_prices/sqlite3_prices.py
@@ -6,29 +6,30 @@ from execute_query import execute_query
 
 
 def main():
-    database = "/Users/marcoguastalli/opt/sqlite/my_python_prices.sqlite"
+    database = "/Users/marcoguastalli/opt/sqlite/prices.sqlite"
 
     conn = create_connection(database)
     try:
         if conn is not None:
             # drop table
-            sql_drop_table = "DROP TABLE prices"
-            execute_query(conn, sql_drop_table)
-            conn.commit()
+            # sql_drop_table = "DROP TABLE prices"
+            # execute_query(conn, sql_drop_table)
+            # conn.commit()
             # create table
-            sql_create_table = '''CREATE TABLE IF NOT EXISTS prices
-                                        (id integer PRIMARY KEY,
-                                        symbol text NOT NULL, 
-                                        amount real NOT NULL, 
-                                        created text NOT NULL)'''
-            execute_query(conn, sql_create_table)
-            conn.commit()
+            # sql_create_table = '''CREATE TABLE IF NOT EXISTS prices
+            #                             (id integer PRIMARY KEY,
+            #                             source text NOT NULL,
+            #                             symbol text NOT NULL,
+            #                             amount real NOT NULL,
+            #                             created text NOT NULL)'''
+            # execute_query(conn, sql_create_table)
+            # conn.commit()
             # first insert
-            query_insert = "INSERT INTO prices VALUES (1, 'BTC', 48000.14, '2021-05-05')"
+            query_insert = "INSERT INTO prices VALUES (2, 'BIN', 'BTC_USDC', 55880.38, CURRENT_TIMESTAMP)"
             execute_query(conn, query_insert)
             conn.commit()
             # select
-            query_select = "SELECT * FROM prices ORDER BY id"
+            query_select = "SELECT * FROM prices ORDER BY created DESC"
             cursor = conn.cursor()
             for row in cursor.execute(query_select):
                 print(row)
