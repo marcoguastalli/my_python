5fc1c2701ffd3234654feb39b747a38e061d6d98
marcoguastalli <marco.guastalli@netcentric.biz>
2021-05-08 14:24:12 +0200

    get_trades

---
+++ cdc_api_client/api/get_trades.py
@@ -0,0 +1,27 @@
+import json
+
+import requests
+from api.api_request import ApiRequest
+
+
+# Fetches the public trades for a particular instrument
+# instrument_name can be omitted to show tickers for all instruments
+# https://exchange-docs.crypto.com/spot/index.html#public-get-trades
+#
+# url = https://api.crypto.com/v2/public/get-trades
+#       https://api.crypto.com/v2/public/get-trades?instrument_name=BTC_USDT
+class GetTrades(ApiRequest):
+    def __init__(self, url):
+        super().__init__(url, None, None)
+
+    def do_get(self):
+        response = None
+        try:
+            headers = {'Content-type': 'application/json'}
+            response = requests.get(self.url, headers=headers)
+
+        except Exception as e:
+            print("Error GetTrades:\n %s" % req)
+            print(e)
+
+        return response
---
+++ cdc_api_client/get_trades.py
@@ -0,0 +1,17 @@
+from api.get_trades import GetTrades
+
+instrument_name = 'BTC_USDT'
+
+
+def main():
+    url = f'https://api.crypto.com/v2/public/get-trades?instrument_name={instrument_name}'
+    trades = GetTrades(url)
+    response = trades.do_get()
+    # response should be type 'requests.models.Response'
+    print("API Response '%s' - '%s'" % (response.status_code, response.reason))
+    response_json_result = response.json()['result']
+    print(response_json_result)
+
+
+if __name__ == "__main__":
+    main()
