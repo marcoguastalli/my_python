6b660e65e5a35be17e706b6168288452c86e5a53
marcoguastalli <marco.guastalli@netcentric.biz>
2021-05-15 16:21:26 +0200

    one DDBB file

--- app_prices/app_prices_calculate.py
+++ app_prices/app_prices_calculate.py
@@ -10,18 +10,16 @@ from db_client.select_query import select_query
 
 def main():
     start = time.time()
-    database_read = "/Users/marcoguastalli/opt/sqlite/prices.sqlite"
-    database_write = "/Users/marcoguastalli/opt/sqlite/variation.sqlite"
+    database = "/Users/marcoguastalli/opt/sqlite/prices.sqlite"
 
-    conn_read = create_connection(database_read)
-    conn_write = create_connection(database_write)
+    conn = create_connection(database)
     try:
-        if conn_read is not None and conn_write is not None:
+        if conn is not None:
             # read prices
-            source = 'CDC'
-            instrument = 'CRO_USDT'
+            source = 'TEST'
+            instrument = 'BTC_USDC'
             query_select = f"SELECT amount from prices where source = '{source}' and instrument = '{instrument}' order by created"
-            rows = select_query(conn_read, query_select)
+            rows = select_query(conn, query_select)
             amount_from = -1
             if rows is not None:
                 for row in rows:
@@ -29,25 +27,27 @@ def main():
                         amount_from = row[0]
                     amount_to = row[0]
                     variation = calculate_variation(amount_from, amount_to)
-                    sql_insert_variation = create_sql_insert_variation(source, instrument, variation)
-                    execute_query(conn_write, sql_insert_variation)
+                    if variation is not None:
+                        sql_insert_variation = create_sql_insert_variation(source, instrument, variation)
+                        execute_query(conn, sql_insert_variation)
 
             # commit
-            conn_write.commit()
+            conn.commit()
             print('calculate execution time: ', time.time() - start, 'seconds')
         else:
             print("Error Connection to DDBB")
     finally:
-        if conn_read is not None:
-            conn_read.close()
-        if conn_write is not None:
-            conn_write.close()
+        if conn is not None:
+            conn.close()
 
 
 def calculate_variation(amount_from, amount_to):
     print(f"amount from: {amount_from}")
     print(f"amount   to: {amount_to}")
-    variation = ((amount_to - amount_from) / amount_from) * 100
+    try:
+        variation = ((amount_to - amount_from) / amount_from) * 100
+    except ZeroDivisionError:
+        return None
     print(f"variation  : {variation}")
     print("")
     return variation
