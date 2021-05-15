3e907d68c407035f93bc877f0a9c7c0d4e93811a
marcoguastalli <marco.guastalli@netcentric.biz>
2021-05-15 22:19:35 +0200

    the following ids should be deleted

--- app_prices/calculate_variation.py
+++ app_prices/calculate_variation.py
@@ -19,9 +19,10 @@ def calculate_variation():
             source_from = -1
             instrument_from = -1
             amount_from = -1
+            id_to_delete = []
 
             # read prices
-            query_select = "SELECT amount, source, instrument from prices order by source, instrument, created ASC"
+            query_select = "SELECT amount, source, instrument, id from prices order by source, instrument, created ASC"
             rows = select_query(conn, query_select)
             if rows is not None:
                 for row in rows:
@@ -30,9 +31,12 @@ def calculate_variation():
                         amount_from = row[0]
                         source_from = row[1]
                         instrument_from = row[2]
+                    # current tuple
                     amount_to = row[0]
                     source = row[1]
                     instrument = row[2]
+                    id_to_delete.append(row[3])
+                    # variation is calculated only when source and instrument coincidono
                     if source_from == source:
                         if instrument_from == instrument:
                             variation = calculate_variation_amount(amount_from, amount_to)
@@ -43,16 +47,20 @@ def calculate_variation():
                                 # set flow-control-variables with current tuple value
                                 amount_from = amount_to
                         else:
+                            # insert the first zero variation for the new instrument
                             sql_insert_variation = create_sql_insert_variation(source, instrument, 0)
                             execute_query(conn, sql_insert_variation)
                             amount_from = row[0]
                     else:
+                        # insert the first zero variation for the new source
                         sql_insert_variation = create_sql_insert_variation(source, instrument, 0)
                         execute_query(conn, sql_insert_variation)
                         amount_from = row[0]
                     # set flow-control-variables with current tuple value
                     source_from = source
                     instrument_from = instrument
+            # the following ids should be deleted
+            print(f"DELETE FROM prices WHERE id IN({id_to_delete}")
             # commit
             conn.commit()
             print('calculate execution time: ', time.time() - start, 'seconds')
