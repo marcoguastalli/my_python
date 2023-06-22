e433e7a9f29958038a186622a9c0a859126835cb
marcoguastalli <marco.guastalli@gmail.com>
2023-06-22 11:06:38 +0200

    Read XML files from:

---
+++ 005_cryptography/000_welcome.py
@@ -0,0 +1,13 @@
+from cryptography.fernet import Fernet
+
+# Put this somewhere safe!
+key = Fernet.generate_key()
+
+f = Fernet(key)
+token = f.encrypt(b"A really secret message. Not for prying eyes.")
+
+token
+b'...'
+
+f.decrypt(token)
+b'A really secret message. Not for prying eyes.'
